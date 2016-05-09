24590b413505488ddf3c77139f0669d622ee9065
remy60 <r.dumoulin@outlook.com>
2016-05-09 15:39:15 +0200

    exo 3 php

---
+++ monPhp3.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+	<head>
+	</head>
+	<body>
+
+          <?php
+          $min = 1;
+         $max = 300;
+   for ($i=$min; $i <$max;$i=$i+2){
+               echo "<p>$i</p>";
+             }
+             ?>
+
+	</body>
+</html>
